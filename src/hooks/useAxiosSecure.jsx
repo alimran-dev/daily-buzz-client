3ce3be1f9b160e9617442f2b48cbea81a58a92d2
alimran-dev <alimran5air@gmail.com>
2023-12-05 09:03:03 +0600

    live baseURL added

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,8 +4,8 @@ import { AuthContext } from "../providers/AuthProvider";
 import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
-    baseURL: "http://localhost:5000",
-    // baseURL: "https://daily-buzz-server.vercel.app",
+    // baseURL: "http://localhost:5000",
+    baseURL: "https://daily-buzz-server.vercel.app",
 })
 
 const useAxiosSecure = () => {
